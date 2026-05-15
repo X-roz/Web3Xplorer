e2599a52909353573afe6d7090ec349f77a004e4
Vignesh S <vigneshkavitha2001@gmail.com>
2026-05-15 20:55:29 +0530

    Web3Xplorer - keccak hash Utils

---
+++ src/main/java/com/web3/learning/utils/KeccakHashUtils.java
@@ -0,0 +1,23 @@
+package com.web3.learning.utils;
+
+import org.bouncycastle.jcajce.provider.digest.Keccak;
+
+import java.nio.charset.StandardCharsets;
+
+public class KeccakHashUtils {
+
+    public static String keccak256(String input) {
+        Keccak.Digest256 digest256 = new Keccak.Digest256();
+        byte[] hashBytes = digest256.digest(input.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(hashBytes);
+    }
+
+    public static String bytesToHex(byte[] inputBytes) {
+        StringBuilder sb = new StringBuilder();
+        for(byte b: inputBytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+}
